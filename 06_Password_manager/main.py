1b791a50219ceaca37a43c9a07cfcba8675e3d01
JoseRopero <josemanuelropero@hotmail.com>
2023-02-05 10:13:32 +0100

    Password manager update

--- 06_Password_manager/main.py
+++ 06_Password_manager/main.py
@@ -1,14 +1,44 @@
 from tkinter import *
+from tkinter import messagebox
+import random
+
+
+def generate_password():
+    letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u',
+               'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P',
+               'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+    numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
+    symbols = ['!', '#', '$', '%', '&', '(', ')', '*', '+']
+
+    nr_letters = random.randint(8, 10)  # Elige un número entre el 8 y el 10
+    nr_symbols = random.randint(2, 4)
+    nr_numbers = random.randint(2, 4)
+
+    lista_letras = [random.choice(letters) for letra in range(nr_letters)]
+    lista_letras += [random.choice(numbers) for numeros in range(nr_numbers)]
+    lista_letras += [random.choice(symbols) for simbolos in range(nr_symbols)]
+
+    random.shuffle(lista_letras)
+    password_final = "".join(lista_letras)
+
+    entry_password.insert(0, password_final)
 
 
 def save():
     web = entry_web.get()
     email = entry_email.get()
     password = entry_password.get()
-    with open("data.txt", "a") as file:
-        file.write(web + " | " + email + " | " + password + "\n")
-    entry_web.delete(0, END)
-    entry_password.delete(0, END)
+
+    if len(web) == 0 or len(email) == 0 or len(password) == 0:  # Para comprobar que no se dejen ningún campo vacio
+        messagebox.showwarning(title="Oops", message="Please don´t leave any fields empty!!")
+    else:
+        is_ok = messagebox.askokcancel(title=web, message=f"These are the details entered:\nEmail: {email}\n"
+                                                          f"Password: {password}\nIs it ok save?")
+        if is_ok:
+            with open("data.txt", "a") as file:
+                file.write(web + " | " + email + " | " + password + "\n")
+            entry_web.delete(0, END)
+            entry_password.delete(0, END)
 
 
 window = Tk()
@@ -31,7 +61,7 @@ label_password.grid(column=0, row=3)
 
 entry_web = Entry(width=45)
 entry_web.focus()
-entry_web.grid(column=1, row=1, columnspan=2)
+entry_web.grid(column=1, row=1, columnspan=2)  # Con columnspan le indicamos el número de columnas que va a ocupar
 
 entry_email = Entry(width=45)
 entry_email.insert(0, "jose@jose.com")
@@ -40,7 +70,7 @@ entry_email.grid(column=1, row=2, columnspan=2)
 entry_password = Entry(width=27)
 entry_password.grid(column=1, row=3)
 
-button_generate = Button(text="Generate Password")
+button_generate = Button(text="Generate Password", command=generate_password)
 button_generate.grid(column=2, row=3)
 
 button_add = Button(text="add", width=36, command=save)
