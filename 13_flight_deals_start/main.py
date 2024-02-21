ef16dae3cb0fc78a476585fa839f878e9ecc101c
JoseRopero <josemanuelropero@hotmail.com>
2024-02-21 23:19:22 +0100

    Flight_deals_sms

--- 13_flight_deals_start/main.py
+++ 13_flight_deals_start/main.py
@@ -6,13 +6,11 @@ from flight_search import FlightSearch
 from datetime import datetime, timedelta
 from notification_manager import NotificationManager
 
-
 data_manager = DataManager()  # Inicializamos el data_manager para el sheet
 sheet_data = data_manager.get_destination_data()
 
 flight_search = FlightSearch()
 notification = NotificationManager()
-
 
 if sheet_data[0]['iataCode'] == "":  # Si la columna esta vacía importamos para recoger los codigos de la ciudad
     for row in sheet_data:
@@ -31,13 +29,11 @@ for destination in sheet_data:
         from_time=tomorrow,
         to_time=six_month
     )
-
+    if flight is None:
+        continue
     # Si el precio es menor que el que tenemos en el sheet enviamos el sms.
     if flight.price < destination['lowestPrice']:
         notification.send_sms(
             message=f'Alerta de precio. Solo {flight.price} libras volar desde {flight.origin_city}-'
                     f'{flight.origin_airport} a {flight.destination_city}-{flight.destination_airport}'
         )
-
-
-
